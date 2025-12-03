517fa192eb564d3d0a24f99f8c1343b3ab0a4cf0
Atmos MAG <zonder2009@gmail.com>
2025-12-03 23:39:14 +0200

    8902

--- VUE_3_Study_02/app.js
+++ VUE_3_Study_02/app.js
@@ -2,6 +2,12 @@ Vue.createApp({
 	data: () => ({
 		myHtml: '<h1>Vue 3 App</h1>',
 		title: 'Data Title',
+		person: {
+			firstName: 'Atmos',
+			lastName: 'Shimora',
+			age: 44
+		},
+		items: [1, 2, 3, 4, 5, 6, 7]
 	})
 
 
