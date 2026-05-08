59b32aeac0b1f8fa6a27b9a0dbb9ff9d2c8aea58
Atmos MAG <zonder2009@gmail.com>
2026-05-08 12:18:36 +0300

    app.js

--- VUE_3_Study_02/app.js
+++ VUE_3_Study_02/app.js
@@ -1,35 +1,35 @@
 Vue.createApp({
-	data: () => ({
-		myHtml: '<h1>Vue 3 App</h1>',
-		title: 'Data Title',
-		person: {
-			firstName: 'Atmos',
-			lastName: 'Shimora',
-			age: 44
-		},
-		items: [1, 2, 3, 4, 5, 6],
-	}),
+    data: () => ({
+        myHtml: '<h1>Vue 3 App</h1>',
+        title: 'Data Title',
+        person: {
+            firstName: 'Atmos',
+            lastName: 'Shimora',
+            age: 45
+        },
+        items: [1, 2, 3, 4, 5, 6],
+    }),
 
-	methods: {
-		addItem(event) {
-			this.items.unshift(this.$refs.myInput.value)
-			this.$refs.myInput.value = ''
-		},
+    methods: {
+        addItem(event) {
+            this.items.unshift(this.$refs.myInput.value)
+            this.$refs.myInput.value = ''
+        },
 
-		remove(idx) {
-			this.items.splice(idx, 1)
-		},
+        remove(idx) {
+            this.items.splice(idx, 1)
+        },
 
-		log(item) {
-			console.log('Log item: ', item)
-		}
-	},
+        log(item) {
+            console.log('Log item: ', item)
+        }
+    },
 
-	computed: {
-		evenItems() {
-			return this.items.filter(i => i % 2 === 0)
-		}
-	}
+    computed: {
+        evenItems() {
+            return this.items.filter(i => i % 2 === 0)
+        }
+    }
 
 
 }).mount('#app')
